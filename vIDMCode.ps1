19233e4d7712ada4d3c7dd0ae13e52b1d3e4e13d
clotero <52245689+clotero@users.noreply.github.com>
2019-06-27 15:05:46 -0400

    Update vIDMCode.ps1

--- vIDMCode.ps1
+++ vIDMCode.ps1
@@ -1,12 +1,34 @@
 # Force TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+#Tenant information
 $vIDMInstance = "https://clotero.vidmpreview.com"
 $acceptType = "application/vnd.vmware.horizon.manager.oauth2client+json"
 $contentType = "application/vnd.vmware.horizon.manager.oauth2client+json"
 
+#Local Directory information
 $localDirectoryName = "API_Directory"
 $localDomainName = "local_domain"
+
+#Local User information
+$user1 = @{
+user = 'jdoe'
+FirstName = 'John'
+LastName = 'Doe'
+Email = 'jdoe@clotero.com'
+UPN = 'jdoe@clotero.com'
+Passord = 'Password1'
+}
+
+$user2 = @{
+user = 'ruser'
+FirstName = 'Random'
+LastName = 'User'
+Email = 'ruser@clotero.com'
+UPN = 'ruser@clotero.com'
+Passord = 'Password2'
+}
+
 
 # To set this up you need to create a service access token.  The information about how to do this is at:
 # https://github.com/vmware/idm/wiki/Integrating-Client-Credentials-app-with-OAuth2
@@ -44,5 +66,30 @@ $webReturn = Invoke-RestMethod -Method Post -Uri $directoryUri -Headers $headers
 
 #Create local users
 
+
+$user = $user1
+$schema = @("urn:scim:schemas:core:1.0","urn:scimschemas:extension:workspace:1.0")
+$userBody = {
+    userName = $user.user
+    "urn:scim:schemas:extension:workspace:1.0" = @{
+        domain = $localDomainName
+    }
+    schemas = $schema
+    password = $user.Passord
+    name = {
+        familyName = $user.LastName;
+        givenName = $user.FirstName
+    }
+    emails = @{
+        
+            value = $user.Email
+        }
+} | ConvertTo-Json
+
+
+
+
+$headers = @{"Authorization" = $userAuthToken; "Content-Type" = $applicationJSON; "Accept" = $applicationJSON}
+$webReturn = Invoke-RestMethod -Method Post -Uri $userUri -Headers $headers -Body $userBody
 
 Write-Output $webReturn
